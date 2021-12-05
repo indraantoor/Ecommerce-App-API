917b6a754c3dba8566df07b042ae99d1048506b4
Indraan S. Toor <indraantoor@gmail.com>
2021-12-06 00:29:20 +0530

    Add: env variables

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Database Connection
 mongoose
-  .connect(
-    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-  )
+  .connect(process.env.MONOGO_URL)
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Backend server is running!");
 });
