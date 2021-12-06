7497a6f3aa35a14ceb61d1a26586027d3859e585
Indraan S. Toor <indraantoor@gmail.com>
2021-12-06 23:22:46 +0530

    Add: user routes and stats feature

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({
-      userName: req.body.user_name,
+      username: req.body.username,
     });
 
     !user && res.status(401).json("Wrong User Name");
