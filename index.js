3f53cae55711208d1d9c7bc71fb7cb5fc4ea188a
Indraan S. Toor <indraantoor@gmail.com>
2021-12-08 00:01:46 +0530

    Fix: stripe integration error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
@@ -9,8 +10,6 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
-
-dotenv.config();
 
 // Database Connection
 mongoose
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,11 +1,24 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
+      description: req.body.description,
+      shipping: {
+        name: "Test User",
+        address: {
+          line1: "Test Line",
+          postal_code: "98140",
+          city: "San Francisco",
+          state: "CA",
+          country: "US",
+        },
+      },
       currency: "usd",
     },
     (stripeErr, stripeRes) => {
